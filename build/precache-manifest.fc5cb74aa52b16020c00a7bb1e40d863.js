e8423baf6a849672b59cff6cd91a0b8ca6503b87
Awais Bin Riaz <awaisbinriaz6@gmail.com>
2023-10-03 09:58:36 +0500

    2ndt commit

---
+++ build/precache-manifest.fc5cb74aa52b16020c00a7bb1e40d863.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b881f834cedb0f6fb10d09e73b37db7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fa986d0b3a48a48536a",
+    "url": "/static/css/2.1ba447cc.chunk.css"
+  },
+  {
+    "revision": "e9f9c06c12c753ec000f",
+    "url": "/static/css/main.b6428bb7.chunk.css"
+  },
+  {
+    "revision": "3fa986d0b3a48a48536a",
+    "url": "/static/js/2.21af523e.chunk.js"
+  },
+  {
+    "revision": "5a995333573cc0e6f689c3983ade882a",
+    "url": "/static/js/2.21af523e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f9c06c12c753ec000f",
+    "url": "/static/js/main.90cb1fee.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.90cb1fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2338da9c9c84a3684af8",
+    "url": "/static/js/runtime-main.6b954d51.js"
+  },
+  {
+    "revision": "0f85a792af6e99f360caccac0e0e1150",
+    "url": "/static/media/G1.0f85a792.jpg"
+  },
+  {
+    "revision": "70ff0ae288002687921a1fb780d8a799",
+    "url": "/static/media/G2.70ff0ae2.jpg"
+  },
+  {
+    "revision": "47723ec4e81fa96034acac835f5bdb7e",
+    "url": "/static/media/G3.47723ec4.jpeg"
+  },
+  {
+    "revision": "f2f205da6d4e004c1dab3534be1d2b27",
+    "url": "/static/media/G4.f2f205da.jpg"
+  },
+  {
+    "revision": "ebb9170fa20e47b9f76642ceb6a1e0ed",
+    "url": "/static/media/G5.ebb9170f.jpg"
+  },
+  {
+    "revision": "7aae83ce4e9d9c3266322cb313b69236",
+    "url": "/static/media/G6.7aae83ce.jpg"
+  },
+  {
+    "revision": "8ec0242e176f20c50895ed4b47b14caa",
+    "url": "/static/media/Rlogo.8ec0242e.png"
+  },
+  {
+    "revision": "93b90a352153d2e3a3d6dc96636fb8be",
+    "url": "/static/media/bg1.93b90a35.jpg"
+  },
+  {
+    "revision": "5477a62a9fb982115072457b464d1faf",
+    "url": "/static/media/bgVideo.5477a62a.mp4"
+  },
+  {
+    "revision": "108313638e3548911192c691ef3ab73f",
+    "url": "/static/media/costumer.10831363.jpg"
+  },
+  {
+    "revision": "07587bd208ba09b11287cde11ed125ee",
+    "url": "/static/media/delivery.07587bd2.jpg"
+  },
+  {
+    "revision": "a6d5a064c4c54d1e90140e610cd1947e",
+    "url": "/static/media/golden-star.a6d5a064.png"
+  },
+  {
+    "revision": "14039cfaf189da8e051ec590a57d46a6",
+    "url": "/static/media/img1.14039cfa.png"
+  },
+  {
+    "revision": "5d04c58805bb70e3c5d817a46c8e5727",
+    "url": "/static/media/img2.5d04c588.png"
+  },
+  {
+    "revision": "e02bf0db0631b4f308b62e6acccca81f",
+    "url": "/static/media/img3.e02bf0db.png"
+  },
+  {
+    "revision": "7d60d70403a695e6b52b4e63b052c980",
+    "url": "/static/media/img4.7d60d704.png"
+  },
+  {
+    "revision": "a7ccc2b522a58b4dca30f07f431bc5dd",
+    "url": "/static/media/img5.a7ccc2b5.png"
+  },
+  {
+    "revision": "b6c5b6baad01e5aa9e873c4f2a375dad",
+    "url": "/static/media/logo2.b6c5b6ba.png"
+  },
+  {
+    "revision": "5fecb18fa1a15673351854ca5d9662b1",
+    "url": "/static/media/quality.5fecb18f.jpg"
+  },
+  {
+    "revision": "4329193cd99fe1d69c5cc8550760d182",
+    "url": "/static/media/services.4329193c.jpg"
+  }
+]);
